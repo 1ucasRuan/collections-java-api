5151561fdf96f18097bcde79e56ad67942757966
1ucasruan <lucasruan1.tuc@gmail.com>
2024-06-19 17:12:36 -0300

    feat: add toString

--- src/map/Pesquisa/Produto.java
+++ src/map/Pesquisa/Produto.java
@@ -30,6 +30,6 @@ public class Produto {
                 "nome='" + nome + '\'' +
                 ", preco=" + preco +
                 ", quantidade=" + quantidade +
-                '}';
+                ' }';
     }
 }
